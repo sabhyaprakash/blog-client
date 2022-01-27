3a375eeb5af014b81f5dbe366049df68574259a9
Sabhya <sabhyaemail@gmail.com>
2022-01-27 11:29:22 +0530

    final one

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
 import {useParams} from 'react-router-dom';
+import ClipLoader from "react-spinners/ClipLoader";
 
 
 
@@ -10,12 +11,16 @@ import {useParams} from 'react-router-dom';
 export default function DetailView({match}) {
     let {id} = useParams();
     const [details, setDetails] = useState({});
+    const [loading, setLoading] = useState(false);
 
 
     useEffect(() => {
+      setLoading(true);
+
         Axios.get(`https://blog-projectly.herokuapp.com/post/${id}`).then((response) => {
     
         console.log(response)
+        setLoading(false)
         setDetails(response.data);
 
         })
@@ -23,84 +28,94 @@ export default function DetailView({match}) {
       } ,[])
 
   return <div>
-       <>
+
+    {loading? 
+    <center>
+     <ClipLoader color={'#F37A24'} loading={loading}  size={100} />
+     </center>
+  
+  :
+  
+  <>
    
-   <h1
-   style={{
-     fontFamily: "Dongle , sans-serif",
-     fontSize: "60px",
-     color: "white",
-  
-position: 'sticky',
-top: 0,
-   }}
- >
-   {details.title}...
- </h1>
+  <h1
+  style={{
+    fontFamily: "Dongle , sans-serif",
+    fontSize: "60px",
+    color: "white",
 
-  
+  }}
+>
+  {details.title}...
+</h1>
 
- <div
-   className="shadow p-3 mb-5 bg-white"
-   style={{
-  
-     borderRadius: "24px",
-
-     borderColor: "white",
-     borderStyle: "solid",
  
-   }}
 
- >
-         <div class="row">
+<div
+  className="shadow p-3 mb-5 bg-white"
+  style={{
  
-         <div class="col-md-5">
-             <div class="row">
-                <div class="col-md-12">
+    borderRadius: "24px",
 
-             
-         <img
-                    src={details.thumbnail}
-                    style={{
-                      borderRadius: "24px 24px 24px 24px",
-                      height: "40vh",
-                      width: '100%'
+    borderColor: "white",
+    borderStyle: "solid",
 
-                    }}
-                    className="img-fluid"
-                    alt="..."
-                  />
-                     </div>
-                     <div class="col-md-7"  style={{
-     fontFamily: "Dongle , sans-serif",
-     fontSize: "60px",
-     color: "black",
+  }}
+
+>
+        <div class="row">
+
+        <div class="col-md-5">
+            <div class="row">
+               <div class="col-md-12">
+
+            
+        <img
+                   src={details.thumbnail}
+                   style={{
+                     borderRadius: "24px 24px 24px 24px",
+                     height: "40vh",
+                     width: '100%'
+
+                   }}
+                   className="img-fluid"
+                   alt="..."
+                 />
+                    </div>
+                    <div class="col-md-7"  style={{
+    fontFamily: "Dongle , sans-serif",
+    fontSize: "60px",
+    color: "black",
+ 
+  }}>
+                     
+                    </div>
+            </div>
+
+                 
+     
+    </div>
+    <div class="col-md-6"  style={{
+    fontFamily: "Dongle , sans-serif",
+    fontSize: "30px",
+
+  }}>
+        {details.content}
+    </div>
+    
+    
+    
+
+
+
+  </div>
   
-   }}>
-                      
-                     </div>
-             </div>
 
-                  
-      
-     </div>
-     <div class="col-md-6"  style={{
-     fontFamily: "Dongle , sans-serif",
-     fontSize: "30px",
-
-   }}>
-         {details.content}
-     </div>
-     
-     
-     
-
-
-
-   </div>
-   
-
- </div>
+</div>
 </>
+  
+  
+  }
+      
   </div>
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -2,14 +2,19 @@ import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import Axios from 'axios';
 import {useHistory} from 'react-router-dom';
+import ClipLoader from "react-spinners/ClipLoader";
 
 
 export default function Item() {
   let history = useHistory();
   const [allPosts, setAllPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     Axios.get("https://blog-projectly.herokuapp.com/allblogs").then((response) => {
       console.log(response);
+      setLoading(false)
       setAllPosts(response.data.reverse());
       
 
@@ -19,110 +24,122 @@ export default function Item() {
   } ,[])
   return (
     <div>
-      <>
-   
-          <h1
-          style={{
-            fontFamily: "Dongle , sans-serif",
-            fontSize: "60px",
-            color: "white",
-         
-  position: 'sticky',
-  top: 0,
-          }}
-        >
-          trending........
-        </h1>
-
-         
-  
-        <div
-          className="shadow p-3 mb-5 bg-white"
-          style={{
-         
-            borderRadius: "24px",
-
-            borderColor: "white",
-            borderStyle: "solid",
-        
-          }}
+      {loading? 
       
-        >
-         
-              {
-              allPosts.map((post)=> {
-            return (
+      <center>
+      <ClipLoader color={'#F37A24'} loading={loading}  size={100} />
+      </center>
+    
+    
+    :
+    <>
+   
+    <h1
+    style={{
+      fontFamily: "Dongle , sans-serif",
+      fontSize: "60px",
+      color: "white",
+   
+position: 'sticky',
+top: 0,
+    }}
+  >
+    trending........
+  </h1>
+
+   
+
+  <div
+    className="shadow p-3 mb-5 bg-white"
+    style={{
+   
+      borderRadius: "24px",
+
+      borderColor: "white",
+      borderStyle: "solid",
+  
+    }}
+
+  >
+   
+        {
+        allPosts.map((post)=> {
+      return (
+  
         
-              
-            
-              <div
-              className="card mb-2 hover shadow-lg p-0 mb-5 bg-white"
-              key = {post._id}
-              onClick={()=> {history.push(`/post/${post._id}`)}}
+      
+        <div
+        className="card mb-2 hover shadow-lg p-0 mb-5 bg-white"
+        key = {post._id}
+        onClick={()=> {history.push(`/post/${post._id}`)}}
+        style={{
+          borderRadius: "24px"
+        }}
+      >
+        
+        <div className="row">
+          <div className="col-md-3">
+            <img
+                   src={post.thumbnail}
               style={{
-                borderRadius: "24px"
+                borderRadius: "24px 0px 0px 24px",
+            height: '23vh',
+            width: '100%'
+             
+        
               }}
-            >
+              className="img-fluid"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-9">
+            <div className="card-body">
+              <h5
+                className="card-title"
+                style={{
+                  fontFamily: "Dongle , sans-serif",
+                  fontSize: "35px"
+                }}
+              >
+                {post.title}
+              </h5>
+<br/><br/>
+              <p className="card-text">
+                <div class="row">
+                  <div class="col-md-11">
+                  <small className="text-muted">
+                  Published on {post.date}
+                </small>
+                    </div>
+                    <div class="col-md-1">
+                    <small className="text-muted">
+                   {post.category}
+                </small>
+                      </div>
+                      </div>
               
-              <div className="row">
-                <div className="col-md-3">
-                  <img
-                         src={post.thumbnail}
-                    style={{
-                      borderRadius: "24px 0px 0px 24px",
-                  height: '23vh',
-                  width: '100%'
-                   
-              
-                    }}
-                    className="img-fluid"
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-9">
-                  <div className="card-body">
-                    <h5
-                      className="card-title"
-                      style={{
-                        fontFamily: "Dongle , sans-serif",
-                        fontSize: "35px"
-                      }}
-                    >
-                      {post.title}
-                    </h5>
-  <br/><br/>
-                    <p className="card-text">
-                      <div class="row">
-                        <div class="col-md-11">
-                        <small className="text-muted">
-                        Published on {post.date}
-                      </small>
-                          </div>
-                          <div class="col-md-1">
-                          <small className="text-muted">
-                         {post.category}
-                      </small>
-                            </div>
-                            </div>
-                    
-                    </p>
-                  </div>
-                </div>
-              </div>
+              </p>
             </div>
-
-          
-            
-            )
-
-
-
-          })}
-  
-          
-  
+          </div>
         </div>
-      </>
+      </div>
+
+    
+      
+      )
+
+
+
+    })}
+
+    
+
+  </div>
+</>
+    
+    
+    }
+     
       
     </div>
     
